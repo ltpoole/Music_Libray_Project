9101394e5530bca0573372eb37e65f8885813db8
ltpoole <laborris.poole@gmail.com>
2023-10-29 23:38:43 -0400

    Initialization of code on GitHub

---
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+    }
+}
